689ae670c127688ce277d8e222214043e18a1b40
luko <luko.web@gmail.com>
2018-12-19 16:36:49 -0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Aprende React - No hace falta refrescar el navegador para que actualice
           </a>
         </header>
       </div>
