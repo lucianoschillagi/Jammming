4d5dfdfb969c3fccc25b7dc8536d45492269254e
luko <luko.web@gmail.com>
2018-12-19 20:50:35 -0300

    Inside of SearchBar.js create a component called SearchBar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,1 +1,15 @@
-jhola
+/* Search Bar Component */
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A Song, Album, or Artist" />
+        <a>SEARCH</a>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
