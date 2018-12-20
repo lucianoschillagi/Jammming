9748a8a0e574f163d05f30729713d13e1a9bd42e
luko <luko.web@gmail.com>
2018-12-19 23:05:01 -0300

    Inside of TrackList.js create a component called TrackList

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -1,1 +1,16 @@
-fjlaskdjk
+/* Tracklist Component */
+import React, { Component } from 'react';
+import './Tracklist.css';
+// TODO: import Track component
+
+class Tracklist extends Component {
+  render() {
+    return (
+      <div className="TrackList">
+      { /* You will add a map method that renders a set of Track components */}
+      </div>
+    );
+  }
+}
+
+export default Tracklist;
