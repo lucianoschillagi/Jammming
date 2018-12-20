d80a9604825c1f3b851cf220daa7f22c8b268a1a
luko <luko.web@gmail.com>
2018-12-19 22:58:02 -0300

    Import Playlist.css into Playlist.js

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,1 +1,17 @@
-hsdflh
+/* Playlist Component */
+import React, { Component } from 'react';
+import './Playlist.css';
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div class="Playlist">
+        <input value="New Playlist"/>
+        {/* Add a TrackList component */}
+        <a class="Playlist-save">SAVE TO SPOTIFY</a>
+    </div>
+    );
+  }
+}
+
+export default Playlist;
