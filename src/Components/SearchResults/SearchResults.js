8642b2bccfaad655387fe714e5ba3c73d1df4d4d
luko <luko.web@gmail.com>
2018-12-30 17:24:11 -0300

    Pass 'search results' to 'track list' in SearchResults component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,18 +1,15 @@
 /* Search Results Component */
 import React from 'react';
 import './SearchResults.css';
+// import Track List
 import TrackList from './TrackList/TrackList';
-
-/*
-Inside the SearchResults.js .render() method, pass this.props.searchResults as 
-an attribute called tracks in the TrackList component.
-*/
 
 class SearchResults extends React.Component {
   render() {
     return (
       <div className="SearchResults" searchResults={this.state.searchResults}>
         <h2>Results</h2>
+        {/* pasa 'search results' a 'track list'*/}
         <TrackList tracks={this.props.searchResults}/>
       </div>
     );
