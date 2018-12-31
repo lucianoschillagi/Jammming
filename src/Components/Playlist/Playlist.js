74c36610ec628845a179fc048bcaf3c550e25d90
luko <luko.web@gmail.com>
2018-12-31 01:20:25 -0300

    Pass the playlist tracks from the Playlist component to the TrackList component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,14 +1,31 @@
 /* Playlist Component */
 import React, { Component } from 'react';
 import './Playlist.css';
-//TODO: import { TrackList } from '';
+// import TrackList component
+import TrackList from '../TrackList/TrackList';
 
+/*
+39.
+Pass the playlist tracks from the Playlist component to the TrackList component.
+
+Hint
+Inside the Playlist.js .render() method, pass this.props.playlistTracks 
+as an attribute called tracks in the TrackList component.
+
+*/
 class Playlist extends Component {
   render() {
     return (
+      
       <div className="Playlist">
+        {/* el nombre del playlist */}
         <input value="New Playlist"/>
-        {/* Add a TrackList component */}
+
+        {/* las lista de tracks */}
+        {/* Pasa el track list dentro del play list */}
+        <TrackList tracks={this.props.playlistTracks}/>
+
+        {/* el botón para guardar el play list actual */}
         <a className="Playlist-save">SAVE TO SPOTIFY</a>
     </div>
     );
