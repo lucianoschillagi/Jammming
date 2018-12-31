0c95f21af268b61bce668174283e21c9008db310
luko <luko.web@gmail.com>
2018-12-31 00:25:31 -0300

    Ends Pass Down Search Result and Render Result List Section

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -3,19 +3,6 @@ import React from 'react';
 import './TrackList.css';
 import Track from '../Track/Track';
 
-/*
-34.
-In the TrackList component, use the .map() method to render 
-each track in the tracks property.
-Set the key attribute to track.id.
-
-Hint
-Inside the TrackList component's render method, use .map() 
-on this.props.tracks to render each track in the list.
-This will require you to pass the current track as 
-an attribute called track to the Track component
-*/
-
 class Tracklist extends React.Component {
   render() {
     return (
