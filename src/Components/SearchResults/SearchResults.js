668f8b04d1db1cc6dea7cb84f1b4f481f6e1ad29
luko <luko.web@gmail.com>
2019-01-02 17:55:33 -0300

    Pass .addTrack() to the SearchResults component as an onAdd attribute.

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -14,7 +14,7 @@ class SearchResults extends React.Component {
     return (
       // le pasa el estado del componente App al componente SearchResults
       // como un atributo llamadado 'searchResults'
-      <div className="SearchResults" searchResults={this.props.searchResults}>
+      <div className="SearchResults" searchResults={this.props.searchResults} onAdd={this.props.addTrack}>
         <h2>Results</h2>
         {/* pasa los resultados de la búsqueda (el array de 
         tracks) a través del atributo ´tracks´ del componente TrackList*/}
