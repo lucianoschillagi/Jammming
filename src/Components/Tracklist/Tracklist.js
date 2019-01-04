88e5622772aa1b2875ef6e09a92bffb5dd4a5ee2
luko <luko.web@gmail.com>
2019-01-04 01:04:37 -0300

    Pass onRemove and isRemoval from the TrackList component to the Track component

--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -6,17 +6,19 @@ import Track from '../Track/Track';
 class TrackList extends React.Component {
   render() {
     return (
-      <div className="TrackList" 
-           isRemoval={false}>
+      <div className="TrackList"
+           isRemoval={true}>
+           onRemove={this.props.onRemove}
       {
          this.props.tracks.map(track => {      
          {/* devuelve un NUEVO array,
-          en este caso un array de [Track] */}     
+          en este caso un array de [Track] */}   
+          // Pass onRemove and isRemoval from the TrackList component to the Track component.  
          return <Track key={track.id} 
                        track={track} 
                        onAdd={this.props.onAdd}
                        onRemove={this.props.onRemove}
-                       isRemoval={true}/>   
+                       isRemoval={this.props.isRemoval}/>   
         }) 
       }
       </div>
