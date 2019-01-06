9260ffb48cfee6915a4043ec151f6c73de6953c9
luko <luko.web@gmail.com>
2019-01-06 01:30:21 -0300

    Add saveUserPlaylist method in Spotify object

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -3,8 +3,41 @@
 const clientID = 'c69500efaf69490a962ae5c955858a60';
 const redirectURI = 'http://localhost:3000/';
 
+
+
 // This object will store the functionality needed to interact with the SPOTIFY API.
 const Spotify = {
+
+  // task: guardar el play list creado por el usuario en su cuenta de Spotify
+  saveUserPlaylist(playlistName, tracks) {
+  /*
+  Create a method in Spotify.js that accepts two arguments. The first argument is the name of the playlist. The second is an array of track URIs.
+
+  Inside the function, check if there are values saved to the method's two arguments. If not, return.
+  */
+
+  // WARNING: preguntar sobre este condicional
+    if (playlistName && tracks) {
+
+    } else {
+      return;
+    }
+
+    /*
+    Create three default variables:
+    An access token variable, set to the current user's access token
+    A headers variable, set to an object with an Authorization parameter containing the user's access token in the implicit grant flow request format
+    An empty variable for the user's ID
+    */
+
+    const accessToken = '00'; // INCOMPLETO
+    const headers = {}; // INCOMPLETO
+    userID;
+
+    
+
+
+  }
   
   // the access token
   token: {},
@@ -12,9 +45,10 @@ const Spotify = {
   // get access token
   // task: obtener el token de acceso a la API
   getAccessToken(token) {
-
     if (token == true) {
       return token
+    } else {
+      return fetch(`https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`);
     }
   },
 
